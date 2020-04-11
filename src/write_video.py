34a550e55044ee99bdd35e6431202b6f15d34eee
loc <mao.l.chong@gmail.com>
2020-04-11 21:08:54 +0800

    get fps width height

--- src/write_video.py
+++ src/write_video.py
@@ -3,7 +3,10 @@ import cv2 as cv
 
 # cap = cv.VideoCapture('data/vtest.avi')
 cap = cv.VideoCapture('/home/lo/Videos/bilibili/asakix264.mp4')
-
+fps = cap.get(cv.CAP_PROP_FPS)
+width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+print(fps, width, height)
 """
     创建一个 VideoWriter 对象。
     我们应该指定输出文件名(例如: output.avi)。
@@ -12,20 +15,19 @@ cap = cv.VideoCapture('/home/lo/Videos/bilibili/asakix264.mp4')
     如果为 True，编码器期望颜色帧，否则它与灰度帧一起工作。
 """
 fourcc = cv.VideoWriter_fourcc(*'x265')
-out = cv.VideoWriter('build/asaki.mp4', fourcc, 20.0, (640,  480))
+out = cv.VideoWriter('build/asaki.mp4', fourcc, fps, (width, height))
 
+ret, frame = cap.read()
 while cap.isOpened():
     ret, frame = cap.read()
     # 如果正确读取帧，ret为True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-
+    # cv.imshow('frame', frame)       # 彩色输出
+    cv.waitKey(int(1000/fps))  # cv.waitKey( )
     out.write(frame)
 
-    # cv.imshow('frame', frame)       # 彩色输出
-    # if cv.waitKey(10) == ord('q'):  # cv.waitKey( )
-    #     break
 cap.release()
 out.release()
 cv.destroyAllWindows()
